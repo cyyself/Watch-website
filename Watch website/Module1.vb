8e6d4a5f6c545fd7852e2c62e8fa18b99c7b8ec0
unknown <chen_yy@outlook.com>
2015-07-07 18:50:41 +0800

    second commit

--- Watch website/Module1.vb
+++ Watch website/Module1.vb
@@ -6,29 +6,40 @@ Module Module1
     Sub Main()
         Console.WriteLine("Enter the url you want to watch:")
         Dim url As String = Console.ReadLine()
-        If url.Contains("http://") Then
+        If url.Contains("http://") Or url.Contains("https://") Then
         Else
             url = "http://" & url
         End If
         Dim last As String = HttpGet(url)
         Console.WriteLine("We will watch this website every 5 second.")
         Do
-            Dim now As String = HttpGet(url)
-            If now = last Then
-                Console.WriteLine("No updates.")
-
+            Dim justnow As String = ""
+            Dim err As Boolean = False
+            Try
+                justnow = HttpGet(url)
+            Catch ex As Exception
+                err = True
+                Console.WriteLine(Now & " Error.")
+            End Try
+            If err Then
             Else
-                If now = "" Then
-                    Console.WriteLine("Error.")
+                If justnow = last Then
+                    Console.WriteLine(Now & " No updates.")
                 Else
-                    Console.WriteLine("The website has been updated.")
-                    Beep()
-                    System.Diagnostics.Process.Start(url)
-                End If
+                    If justnow = "" Then
+                        Console.WriteLine(Now & " Error.")
+                    Else
+                        Console.WriteLine(Now & " The website has been updated.")
+                        Beep()
+                        System.Diagnostics.Process.Start(url)
+                        Exit Do
+                    End If
 
+                End If
+                System.Threading.Thread.Sleep(5000)
             End If
-            System.Threading.Thread.Sleep(5000)
         Loop
+        Console.ReadKey()
     End Sub
     Function HttpGet(URL As String) As String
         Dim request As WebRequest = WebRequest.Create(URL)
